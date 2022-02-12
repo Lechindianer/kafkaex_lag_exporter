c584c6dbeee14dbcccd1a2d89339ffb2521fdc71
Pascal Schmid <psschmid@gmx.de>
2022-02-12 23:14:35 +0100

    Install kafka_ex

--- mix.exs
+++ mix.exs
@@ -23,6 +23,7 @@ defmodule KafkaexLagExporter.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:kafka_ex, "~> 0.12.1"},
     ]
   end
 end
