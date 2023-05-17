bdb892efecc29e67be3aa7d303b1fa7bd7870738
Pascal Schmid <psschmid@gmx.de>
2023-05-17 08:06:47 +0200

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -33,14 +33,14 @@ defmodule KafkaexLagExporter.MixProject do
   defp deps do
     [
       {:phoenix, "~> 1.6.6"},
-      {:telemetry_metrics, "~> 0.6"},
+      {:telemetry_metrics, "~> 0.6.1"},
       {:telemetry_poller, "~> 1.0"},
-      {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"},
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:jason, "~> 1.4.0"},
+      {:plug_cowboy, "~> 2.6.1"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
-      {:brod, "~> 3.16"},
-      {:prom_ex, "~> 1.6.0"}
+      {:brod, "~> 3.16.1"},
+      {:prom_ex, "~> 1.8.0"}
     ]
   end
 end
